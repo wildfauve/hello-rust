8b99ae38276a8099b806e0cb7e7ff4c624830d34
Col Perks <wild.fauve@gmail.com>
2025-05-08 08:25:28 +1200

    remove target

--- target/debug/deps/hello_world-2b8c25dc2411a110.d
+++
@@ -1,5 +0,0 @@
-/Users/colperks/lang/Learning/rust/hello-world/target/debug/deps/hello_world-2b8c25dc2411a110: src/main.rs
-
-/Users/colperks/lang/Learning/rust/hello-world/target/debug/deps/hello_world-2b8c25dc2411a110.d: src/main.rs
-
-src/main.rs:
